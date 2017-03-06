c640151b96fb78cccc94034261e528f4b35fd7b8
archi77 <dibeluy@gmail.com>
2017-03-06 14:42:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserCollection<string> bc = new UserCollection<string>();
+            bc.FillList("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eigth", "Nine", "Ten");
+
+            foreach (string word in bc)
+                Console.WriteLine(word);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserCollection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class UserCollection<T>
+    {
+        private List<T> data = new List<T>();
+
+        public T this[int Index]
+        {
+            get { return data[Index]; }
+            set { data[Index] = value; }
+        }
+
+        public void FillList(params T[] items)
+        {
+            foreach (var datum in items)
+            {
+                data.Add(datum);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var datum in data)
+            {
+                yield return datum;
+            }
+        }
+    }
+}
